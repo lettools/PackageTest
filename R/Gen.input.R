77383a90a10491a550e9ff1270b53629b4c29816
AC1990 <arclement@hotmail.com>
2018-02-27 12:29:12 +0000

    Update Gen.input.R

--- R/Gen.input.R
+++ R/Gen.input.R
@@ -66,8 +66,8 @@ Gen.input <- function(Chromosome, ASE_file, legend_file, haplotypes_file, Sample
                p", "POPFIN_p", "POPGBR_p", "POPTSI_p", "Variant_p", "TSS", "Gene.x")
     
     # 6. Load input files                                 
-    Samples<-read.table(Samples, header=T)
-    cols <- colnames(Samples)
+    Samples<-read.table(Samples_file, header=T)
+    cols <- colnames(Samples_file)
     if (!"ID" %in% cols | !"SEX" %in% cols | !"POP" %in% cols)
     {
       ID <- c("HG00096", "HG00097", "HG00099", "NA20827") 
