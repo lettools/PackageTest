c2aff22efe8e5011294c10b3e9feb757cf4feca0
AC1990 <arclement@hotmail.com>
2018-03-06 11:29:50 +0000

    Update Run.model.R

--- R/Run.model.R
+++ R/Run.model.R
@@ -44,7 +44,7 @@ Run.Model <- function(input_file, Task, progress_path, numTasks=100, Chromosome,
   
   # Model to run analysis
   
-  tryCatch(library(GenomicRanges))
+  tryCatch(library(GenomicRangesf))
   library(GenomicRanges)
   library(reshape2)
   library(rtracklayer)
